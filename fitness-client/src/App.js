2c045033b65de9bf424115ba9b342e909356af6e
Keith Willis <williskeith04@gmail.com>
2020-07-06 14:00:02 -0500

    added component

--- fitness-client/src/App.js
+++ fitness-client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import NavBar from "./components/NavBar";
+import CalorieTracker from "./components/CalorieTracker";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <CalorieTracker />
     </div>
   );
 }
---
+++ fitness-client/src/components/CalorieTracker.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CalorieTracker() {
+  return <h1>Hello</h1>;
+}
+
+export default CalorieTracker;
