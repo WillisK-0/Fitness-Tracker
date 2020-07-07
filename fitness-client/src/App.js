7de2872b39f5e448477b1248601b34aad79fb32e
Nathan Orris <Nathan_NoSudo@codebloodeddev.com>
2020-07-07 14:27:19 -0500

    added too app.js a div for searchingand 2 new imports

--- fitness-client/src/App.js
+++ fitness-client/src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
+import React, { useContext } from 'react';
 import logo from "./logo.svg";
-
+import RecipeList from 'fitness-client/src/components/RecipeList';
+import { RecipeContext } from './context/index'
 import NavBar from "./components/NavBar";
 import CalorieTracker from "./components/CalorieTracker";
 
 import "./App.css";
 
 function App(props) {
+  const appContext = useContext(RecipeContext)
+  const { loading, search } = appContext
   return (
     <div>
       <NavBar />
     {props.children}
      
     </div>
+    <div>
+    {loading ? <h1 className="text-center">...fetching {search} recipe</h1> :
+      <RecipeList />}
+  </div>
   );
 
 }
