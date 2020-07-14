81c391e789abfc6c44bb33820898dc3ce8e3be58
Keith Willis <williskeith04@gmail.com>
2020-07-13 23:20:14 -0500

    styled search functions

--- fitness-client/src/components/RecipeSearchResults.js
+++ fitness-client/src/components/RecipeSearchResults.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
+import "../style/recipeSearchResults.css";
 
 function RecipeSearchResults(props) {
   return (
     <>
-      <div>
-        <ul>
+      <div className="results-wrapper">
+        <ul className="results-items">
           {props.recipeSearchList.map((item, index) => {
             return (
-              <li>
+              <li className="results-item">
                 <NavLink to={"results/" + index}>
                   <div className="food-card">
-                    <img src={item.recipe.image}></img>
+                    <img src={item.recipe.image} id="food-image"></img>
                     <figcaption>{item.recipe.label}</figcaption>
                     <input type="hidden" value={index}></input>
                   </div>
