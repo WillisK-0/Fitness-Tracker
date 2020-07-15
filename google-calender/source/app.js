40b82826711ee0b5b43d13647da3fc7f15c8a6b3
NathanNOSudo <nathandavid719@gmail.com>
2020-07-15 17:32:54 -0400

    added api auth for calendar and set up ajax for fetching events.

--- google-calender/source/app.js
+++ google-calender/source/app.js
@@ -1,2 +1,38 @@
-const who = 'WorldStarHipHop'
-console.log(`Hello ${who}`)
+// First, I need React itself, and React-DOM (which lets React interact with HTML).
+import React from 'react'
+import { render } from 'react-dom';
+// Then here is App, which is just like a React.Component (a blueprint for a component), but additionally: it’s state consists of a string “React” and it renders a div element with a string “Hello ” plus a string from the state
+import moment from 'moment';
+
+import BigCalendar from 'react-big-calendar';
+// a localizer for BigCalender
+BigCalendar.momentLocalizer(moment)
+import { getEvents } from './gcal'
+
+// This weird syntax is just a shorthand way of specifying loaders.
+require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
+
+class App extends React.Component {
+    constructor () {
+      super()
+      this.state = {
+        events: []
+      }
+    }
+    componentDidMount () {
+        getEvents((events) => {
+          this.setState({events})
+        })
+      }
+    render() {
+        return (
+            // Remember React components in jsx look like HTML tags
+            <BigCalendar 
+              style={{height: '420px'}}
+              events={[]}
+            />
+        )
+    }
+}
+// Finally, please render this component inside of element with id "root"
+render(<App />, document.getElementById('root')) 
